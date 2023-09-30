eccc3d54ad24810fe933efc74673dfa9373576e5
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-30 12:13:08 +0530

    jenkins

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -21,6 +21,9 @@ def call() {
             }
 
             stage('Code Security') {
+                when {
+                    expression { BRANCH_NAME == "main"}
+                }
                 steps {
                     echo 'Hello World'
                 }
